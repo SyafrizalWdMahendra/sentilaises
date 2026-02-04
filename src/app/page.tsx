d2c7254e6aeff85525d07feff7679ac7d7237852
Mahen <mahennekkers27@gmail.com>
2026-02-04 10:31:43 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { LoginPage } from "./auth/login/page";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginPage />
+      </div>
+    </div>
+  );
+}
