c75f17184a32a6ddd65699b03a35260767c80010
Mahen <mahennekkers27@gmail.com>
2026-03-03 10:16:43 +0700

    chore: adjust input validation

--- src/app/validation/analyze.schema.ts
+++ src/app/validation/analyze.schema.ts
@@ -3,6 +3,6 @@ import z from "zod";
 export const analyzeSchema = z.object({
   url1: z.string().min(10, "Tautan 1 minimal 10 karakter"),
   url2: z.string().min(10, "Tautan 2 minimal 10 karakter"),
-  url3: z.string().min(10, "Tautan 3 minimal 10 karakter"),
+  url3: z.string().optional().or(z.literal("")),
   url4: z.string().optional().or(z.literal("")),
 });
