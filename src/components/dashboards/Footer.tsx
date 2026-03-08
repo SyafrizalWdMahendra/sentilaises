a95121e43c1bd46e4614568bb6dca081f99e7cc3
Mahen <mahennekkers27@gmail.com>
2026-03-08 09:58:50 +0700

    style: change footer titile value

--- src/components/dashboards/Footer.tsx
+++ src/components/dashboards/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="flex flex-col items-center justify-between gap-4 text-sm text-muted-foreground sm:flex-row">
         <div>
           <p className="font-medium text-foreground text-center lg:text-start md:text-start">
-            SentiLaptop - Analisis Sentimen
+            SENTILAISES.
           </p>
           <p>Skripsi oleh Syafrizal Wd Mahendra (E41222719)</p>
         </div>
